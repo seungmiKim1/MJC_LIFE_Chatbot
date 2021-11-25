21ab5121a0a60bdf47306f54dc7ab47116bf62b2
seungmi kim <89903766+seungmiKim1@users.noreply.github.com>
2021-11-26 02:43:42 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var users = {};
+
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', function(socket){
+    console.log('사용자가 채팅에 연결되었습니다.');
+
+    socket.on('disconnect', function(){
+        console.log('사용자와 채팅의 연결이 끊어졌습니다.');
+    });
+
+    socket.on("USERMSG", (data)=>{
+        console.log(data);
+        io.emit("SERVERMSG", data);
+      
+    })
+
+    socket.on("SETNICKNAME", (nickname)=>{
+        users[nickname] = socket;
+        console.log( nickname);
+    })
+});
+
+http.listen(3000, function(){
+    console.log('listening on port 3000');
+});
